9055c8dfc3947332d9be9c5aa0a0ed8b84acaf36
Anushka6060 <kushananushka12345@gmail.comm>
2024-10-01 11:47:22 +0530

    error fixed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
+import { ScrollToTopService } from './shared/util/ScrollToTopService';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
+  provideHttpClient(withFetch()),
+  provideRouter(
+    routes,
+    withInMemoryScrolling({
+      scrollPositionRestoration: "top",
+    })
+  ),
+    ScrollToTopService]
 };
