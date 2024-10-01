54a99c71bf439588337e4aa900c0b9a5d58454d9
Anushka6060 <kushananushka12345@gmail.comm>
2024-10-01 12:06:16 +0530

    eroor fix2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,16 +5,18 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
 import { ScrollToTopService } from './shared/util/ScrollToTopService';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
-  provideHttpClient(withFetch()),
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(withFetch()),
   provideRouter(
     routes,
     withInMemoryScrolling({
       scrollPositionRestoration: "top",
     })
   ),
-    ScrollToTopService]
+    ScrollToTopService],
+
+
 };
