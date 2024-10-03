45f673d23ecae2840cb592da6620dddaf1e0560d
Anushka6060 <kushananushka12345@gmail.com>
2024-10-03 07:15:22 +0530

    ui changes

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './service.component.html',
   styleUrl: './service.component.scss'
 })
 export class ServiceComponent {
+  @Input() uiChnages = false
 
 }
